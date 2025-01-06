443d6fd46f6cd0e5cdaf5e1d9530ff8414211e58
Gaurav Sharma <131950180+gauravsharma5166@users.noreply.github.com>
2025-01-06 22:40:55 +0530

    reorganized components

---
+++ elib/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Banner from "@/app/(home)/components/Banner";
+import ProductList from "./components/ProductList";
+
+export default function Home() {
+  return (
+    <>
+      <Banner/>
+      <ProductList/>
+    </>
+     
+  );
+}
