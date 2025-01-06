4aca9df2a881052cfcd9bafa69cb4b26cb32a32d
Gaurav Sharma <131950180+gauravsharma5166@users.noreply.github.com>
2025-01-06 21:09:01 +0530

    added banner

--- elib/app/page.tsx
+++ elib/app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "@/Components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>this is e library</h1>
+      <Banner/>
     </>
      
   );
